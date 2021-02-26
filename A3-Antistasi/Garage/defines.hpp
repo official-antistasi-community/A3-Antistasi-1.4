48c34253f68d4553df5a590ea2f3fd019d27fde2
Håkon <hrr1@hotmail.no>
2021-02-27 00:53:05 +0100

    removed old unused pixel grid stuff

--- A3-Antistasi/Garage/defines.hpp
+++ A3-Antistasi/Garage/defines.hpp
@@ -104,25 +104,6 @@
 #define MB_BUTTON_OK      1
 #define MB_BUTTON_CANCEL  2
 #define MB_BUTTON_USER    4
-//-- WIP Pixel Grid --\\
-// Default grid
-#define GUI_GRID_WAbs       ((safezoneW / safezoneH) min 1.2)
-#define GUI_GRID_HAbs       (GUI_GRID_WAbs / 1.2)
-#define GUI_GRID_W          (GUI_GRID_WAbs / 40)
-#define GUI_GRID_H          (GUI_GRID_HAbs / 25)
-#define GUI_GRID_X          (safezoneX)
-#define GUI_GRID_Y          (safezoneY + safezoneH - GUI_GRID_HAbs)
-
-// Default text sizes
-#define GUI_TEXT_SIZE_SMALL  (GUI_GRID_H * 0.8)
-#define GUI_TEXT_SIZE_MEDIUM (GUI_GRID_H * 1)
-#define GUI_TEXT_SIZE_LARGE  (GUI_GRID_H * 1.2)
-
-// Pixel grid
-#define pixelScale  0.50
-#define GRID_W (pixelW * pixelGrid * pixelScale)
-#define GRID_H (pixelH * pixelGrid * pixelScale)
-//-- WIP Pixel Grid --\\
 
 //standardised values
 #define FontSize "safeZoneW / safeZoneH min 1.2 / 1.2 / 25"

27e39b87df62edf5cd0e6bce0145ecd7cafcd64a
Bob-Murphy <50651578+Bob-Murphy@users.noreply.github.com>
2021-04-25 00:40:39 +0200

    fixed typo

--- A3-Antistasi/MissionDescription/CfgRemoteExec.hpp
+++ A3-Antistasi/MissionDescription/CfgRemoteExec.hpp
@@ -1,7 +1,7 @@
 class CfgRemoteExec {
     // List of Functions allowed to be sent from client via remoteExec
     class Functions {
-        mode = 2;           // 0: Completely blocked,   1: Blocked by defualt,      2: Allow All
+        mode = 2;           // 0: Completely blocked,   1: Blocked by default,      2: Allow All
         jip = 1;            // 0: No JIP,               1: JIP Allowed
         allowedTargets = 0; // 0: All machines,         1: Only to other clients,   2: Only to server
 
@@ -9,7 +9,7 @@ class CfgRemoteExec {
 
     // List of Commands allowed to be sent from client via remoteExec
     class Commands {
-        mode = 2;           // 0: Completely blocked,   1: Blocked by defualt,      2: Allow All
+        mode = 2;           // 0: Completely blocked,   1: Blocked by default,      2: Allow All
         jip = 1;            // 0: No JIP,               1: JIP Allowed
         allowedTargets = 0; // 0: All machines,         1: Only to other clients,   2: Only to server
 

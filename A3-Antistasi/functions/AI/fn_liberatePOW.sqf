mimizid/A3-Antistasi

Multilanguage support - groupChat\sideChat\globalChat

A3-Antistasi/functions/AI/fn_liberatePOW.sqf
@@ -11,11 +11,11 @@ if (captive _playerX) then
 	_playerX setCaptive false;
 	};
 
-_playerX globalChat "You are free. Come with us!";
+_playerX globalChat localize "STR_antistasi_chat_liberate_free";
 _unit setDir (getDir _playerX);
 _playerX playMove "MountSide";
 sleep 3;
-_unit sideChat "Thank you. I owe you my life!";
+_unit sideChat localize "STR_antistasi_chat_liberate_thank";
 
 _unit enableAI "MOVE";
 _unit enableAI "AUTOTARGET";

official-antistasi-community/A3-Antistasi-1.4

A3-Antistasi/functions/AI/fn_liberatePOW.sqf
@@ -2,7 +2,7 @@ params ["_unit", "_playerX"];
 
 if (captive _playerX) then { _playerX setCaptive false };
 
-_playerX globalChat "You are free. Come with us!";
+_playerX globalChat localize "STR_antistasi_chat_liberate_free";
 _unit setDir (getDir _playerX);
 _playerX playMove "MountSide";
 sleep 5;
@@ -15,7 +15,7 @@ if (_timeout < 0) exitWith {};
 
 [_unit,"remove"] remoteExec ["A3A_fnc_flagaction",[teamPlayer,civilian],_unit];
 
-_unit globalChat "Thank you. I owe you my life!";
+_unit globalChat localize "STR_antistasi_chat_liberate_thank";
 _unit enableAI "MOVE";
 _unit enableAI "AUTOTARGET";
 _unit enableAI "TARGET";
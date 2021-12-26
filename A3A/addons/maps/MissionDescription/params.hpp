11ed0d32572dbd8d201aec9f730c61aec3625fd0
killerswin2 <thecakebecamethelie@yahoo.com>
2021-12-26 01:48:07 -0600

    autoPick now works with cup

--- A3A/addons/maps/MissionDescription/params.hpp
+++ A3A/addons/maps/MissionDescription/params.hpp
@@ -238,31 +238,31 @@ class Params
     class RebelFactionEnum
     {
         title = "Rebel faction (Required content for faction in parentheses)";
-        values[] = {9999, 1,2,3,4,5,6,7,8};
+        values[] = {9999, 1,2,3,4,5,6,7,8,9};
         texts[] = {"Default (Auto)", "FIA", "SDK", "NAPA (RHS)", "CNM (Factions)", "TKM (Factions)", "POF (SOG:PF)", "NPC (CUP)", "TKL (CUP)"};
         default = 9999;
     };
     class OccupantsFactionEnum
     {
         title = "Occupants faction (Required content for faction in parentheses)";
-        values[] = {9999, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+        values[] = {9999, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29};
         texts[] = {
             "Default (Auto)", "NATO", "CSAT", "AAF", "LDF (Contact)", "USAF (RHS)", "AFRF (RHS)","CDF (RHS)"
             , "US Marines (RHS)", "BAF (BAF)", "Coldwar US (Factions)", "Coldwar Soviets (Factions)"
             , "TKA West (Factions)", "TKA East (Factions)", "ADA (Factions)", "ANA (Factions)", "AAF (Factions)", "MDF (Factions)", "HIDF (Factions)", "MACV (SOG:PF)", "PAVN (SOG:PF)"
-            , "ACR (CUP)", "BAF (CUP)", "CDF (CUP)", "USAF (CUP)", "USMC (CUP)", "AFRF (CUP)", "RAS (CUP)", "SLA (CUP)", "TA (CUP)", "BW (CUP:BWMOD)"
+            , "ACR (CUP)", "BAF (CUP)", "CDF (CUP)", "USAF (CUP)", "USMC (CUP)", "AFRF (CUP)", "RAS (CUP)", "SLA (CUP)", "TA (CUP)"
         };
         default = 9999;
     };
     class InvadersFactionEnum
     {
         title = "Invaders faction (Required content for faction in parentheses)";
-        values[] = {9999, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+        values[] = {9999, 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29};
         texts[] = {
             "Default (Auto)", "NATO", "CSAT", "AAF", "LDF (Contact)", "USAF (RHS)", "AFRF (RHS)","CDF (RHS)"
             , "US Marines (RHS)", "BAF (BAF)", "Coldwar US (Factions)", "Coldwar Soviets (Factions)"
             , "TKA West (Factions)", "TKA East (Factions)", "ADA (Factions)", "ANA (Factions)", "AAF (Factions)", "MDF (Factions)", "HIDF (Factions)", "MACV (SOG:PF)", "PAVN (SOG:PF)"
-            ,"ACR (CUP)", "BAF (CUP)", "CDF (CUP)", "USAF (CUP)", "USMC (CUP)", "AFRF (CUP)", "RAS (CUP)", "SLA (CUP)", "TA (CUP)", "BW (CUP:BWMOD)"
+            ,"ACR (CUP)", "BAF (CUP)", "CDF (CUP)", "USAF (CUP)", "USMC (CUP)", "AFRF (CUP)", "RAS (CUP)", "SLA (CUP)", "TA (CUP)"
         };
         default = 9999;
     };

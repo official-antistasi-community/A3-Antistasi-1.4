b425f89f8553d7337ec4c74d1808ff77915f1673
Caleb Serafin <calebserafin@outlook.com>
2022-01-26 23:18:45 +0200

    🧱 Created GUI addon meta.

---
+++ A3A/addons/GUI/CfgFunctions.hpp
@@ -0,0 +1,6 @@
+// note use of preInit & postInit will run for EVERY mission, use sparingly or with non a3a mission aborts in place, example check if the class (missionConfigFile >> "A3A") exists
+class CfgFunctions {
+    class A3A {
+
+    };
+};

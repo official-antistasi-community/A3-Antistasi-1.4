217e47211721577294150cbc9ef3ac069f40df36
Bob Murphy <bobmurphy@gmx.de>
2022-02-18 20:52:35 +0100

    malden update
    Disabled village Malden_L_Moray
    updated fuelstation list to handle all BI fuelstations

--- A3A/addons/maps/Antistasi_Malden.Malden/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_Malden.Malden/mapInfo.hpp
@@ -1,10 +1,12 @@
 class malden {
-	population[] = {};
-	disabledTowns[] = {"Malden_C_Airport"};
+	population[] = {}; //automated data is fine and needs no adaptation
+	disabledTowns[] = {"Malden_C_Airport","Malden_L_Moray"};
 	antennas[] = {};
 	antennasBlacklistIndex[] = {};
-	banks[] = {};
+	banks[] = {}; //only one possible building but directly next to a factory
 	garrison[] = {{},{"airport","seaport_7"},{},{}};
-	fuelStationTypes[] = {"Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_Feed_F"};
+	fuelStationTypes[] = {
+		"Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F"
+	};
 	climate = "arid";
 };

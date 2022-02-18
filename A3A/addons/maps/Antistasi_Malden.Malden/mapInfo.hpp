c9df918fde86766c93b0112e5cc122684e4c66a0
Bob Murphy <bobmurphy@gmx.de>
2022-02-18 21:16:34 +0100

    malden antenna update
    added antenna list and blacklisted 2

--- A3A/addons/maps/Antistasi_Malden.Malden/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_Malden.Malden/mapInfo.hpp
@@ -1,8 +1,10 @@
 class malden {
 	population[] = {}; //automated data is fine and needs no adaptation
 	disabledTowns[] = {"Malden_C_Airport","Malden_L_Moray"};
-	antennas[] = {};
-	antennasBlacklistIndex[] = {};
+	antennas[] = {
+		{5692.11,5551.59,1.4357},{4744.17,7156.41,0.00292969},{7057.21,9932.39,3.05176e-005},{7000.77,10033.3,0.808578},{8309.54,10138.9,-0.000183105},{8309.54,10138.9,5.72205e-005},{4967.98,2280.99,0.0695496},{9635.35,3309.94,0.510147},{11320.1,4122.02,0.194778}
+	};
+	antennasBlacklistIndex[] = {4,6};
 	banks[] = {}; //only one possible building but directly next to a factory
 	garrison[] = {{},{"airport","seaport_7"},{},{}};
 	fuelStationTypes[] = {

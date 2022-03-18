9f870a31677ac2cb4d7b91eb39d257eeb4b4376e
Bob Murphy <bobmurphy@gmx.de>
2022-03-18 23:41:03 +0100

    virolahti update
    disable nopala #2221

--- A3A/addons/maps/Antistasi_vt7.vt7/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_vt7.vt7/mapInfo.hpp
@@ -1,8 +1,8 @@
 class vt7 {
 	population[] = {
-		{"aarre",80},{"Alapihlaja",90},{"Eerikkala",88},{"haavisto",60},{"Hailila",90},{"Hanski",100},{"Harju",100},{"harjula",70},{"Hurppu",80},{"Hyypianvuori",60},{"Jarvenkyla",100},{"Kirkonkyla",400},{"Klamila",150},{"Koivuniemi",100},{"Korpela",80},{"Kouki",90},{"Lansikyla",100},{"Myllynmaki",60},{"Nakarinmaki",90},{"Niemela",60},{"nopala",80},{"Ojala",80},{"Onnela",100},{"Pajunlahti",90},{"piispa",100},{"Pyterlahti",190},{"Rannanen",80},{"Ravijoki",90},{"Riko",100},{"Santaniemi",100},{"Skippari",80},{"suopelto",80},{"Sydankyla",150},{"uski",80},{"Uutela",100},{"Vilkkila",110},{"Virojoki",500},{"Ylapaa",80},{"Ylapihlaja",80},{"Souvio",70}
+		{"aarre",80},{"Alapihlaja",90},{"Eerikkala",88},{"haavisto",60},{"Hailila",90},{"Hanski",100},{"Harju",100},{"harjula",70},{"Hurppu",80},{"Hyypianvuori",60},{"Jarvenkyla",100},{"Kirkonkyla",400},{"Klamila",150},{"Koivuniemi",100},{"Korpela",80},{"Kouki",90},{"Lansikyla",100},{"Myllynmaki",60},{"Nakarinmaki",90},{"Niemela",60},{"Ojala",80},{"Onnela",100},{"Pajunlahti",90},{"piispa",100},{"Pyterlahti",190},{"Rannanen",80},{"Ravijoki",90},{"Riko",100},{"Santaniemi",100},{"Skippari",80},{"suopelto",80},{"Sydankyla",150},{"uski",80},{"Uutela",100},{"Vilkkila",110},{"Virojoki",500},{"Ylapaa",80},{"Ylapihlaja",80},{"Souvio",70}
 	};
-	disabledTowns[] = {"Tikanen","toipela","hirvela","kallio","Kuusela"};
+	disabledTowns[] = {"Tikanen","toipela","hirvela","kallio","Kuusela","nopala"};
 	antennas[] = {
 		{907.35,2955.65,0},{6644.62,7275.58,0.00256348},{6242.47,13009.4,0.39426},{13061.2,6487.81,0.760155},{1768.36,15526.1,0.00277328},{15449.2,16603.3,0}
 	};

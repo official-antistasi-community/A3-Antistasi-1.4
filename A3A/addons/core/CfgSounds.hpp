510f09cbae32cd35323fe14a834b8ed8f89f8bfd
Whiskeysim(Hazey) <1194692+MeltedPixel@users.noreply.github.com>
2022-03-22 22:16:45 -0700

    Standardization of CfgSounds

--- A3A/addons/core/CfgSounds.hpp
+++ A3A/addons/core/CfgSounds.hpp
@@ -56,7 +56,7 @@ class CfgSounds
         sound[] = {EQPATHTOFOLDER(core,Sounds\Misc\earthquake1.wss), db + 45, 1};
         titles[] = {};
     };
-	
+
 	class A3A_Audio_Civ_Song1
 	{
 		name = "A3A_Audio_Civ_Song1";
@@ -74,28 +74,28 @@ class CfgSounds
 	class A3A_Audio_Civ_Song3
 	{
 		name = "A3A_Audio_Civ_Song3";
-		sound[] = {QPATHTOFOLDER(Sounds\Civilian\song3.ogg), 0.5, 1, 20};
+		sound[] = {EQPATHTOFOLDER(core,Sounds\Civilian\song3.ogg), 0.5, 1, 20};
 		titles[] = {};
 	};
 
 	class A3A_Audio_Civ_Song4
 	{
 		name = "A3A_Audio_Civ_Song4";
-		sound[] = {QPATHTOFOLDER(Sounds\Civilian\song4.ogg), 0.5, 1, 20};
+		sound[] = {EQPATHTOFOLDER(core,Sounds\Civilian\song4.ogg), 0.5, 1, 20};
 		titles[] = {};
 	};
 
 	class A3A_Audio_Civ_Song5
 	{
 		name = "A3A_Audio_Civ_Song5";
-		sound[] = {QPATHTOFOLDER(Sounds\Civilian\song5.ogg), 0.5, 1, 20};
+		sound[] = {EQPATHTOFOLDER(core,Sounds\Civilian\song5.ogg), 0.5, 1, 20};
 		titles[] = {};
 	};
 
 	class A3A_Audio_Civ_Song6
 	{
 		name = "A3A_Audio_Civ_Song6";
-		sound[] = {QPATHTOFOLDER(Sounds\Civilian\song6.ogg), 0.5, 1, 20};
+		sound[] = {EQPATHTOFOLDER(core,Sounds\Civilian\song6.ogg), 0.5, 1, 20};
 		titles[] = {};
 	};
 };

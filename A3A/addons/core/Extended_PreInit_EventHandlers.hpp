da14a334c2bcf40d89ded965389db940794f171d
Whiskeysim(Hazey) <1194692+MeltedPixel@users.noreply.github.com>
2022-04-16 11:37:42 -0700

    Debug Disable for testing

--- A3A/addons/core/Extended_PreInit_EventHandlers.hpp
+++ A3A/addons/core/Extended_PreInit_EventHandlers.hpp
@@ -3,6 +3,6 @@ class Extended_PreInit_EventHandlers
     class VCom_init_event 
 	{
 		preinit = 1;
-        init = "call compile preprocessFileLineNumbers 'vcomai\Functions\VCM_Functions\fn_CBASettings.sqf';";
+        init = "call compile preprocessFileLineNumbers QPATHTOFOLDER(vcomai\Functions\VCM_Functions\fn_CBASettings.sqf);";
     };
 };

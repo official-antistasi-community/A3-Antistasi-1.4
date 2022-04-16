f6e176b8d046cc18cc6eb3f6334e0f5d84b02cb1
Whiskeysim(Hazey) <1194692+MeltedPixel@users.noreply.github.com>
2022-04-16 13:04:07 -0700

    Enabled Debug, Minor Fixes

--- A3A/addons/maps/MissionDescription/CfgRemoteExec.hpp
+++ A3A/addons/maps/MissionDescription/CfgRemoteExec.hpp
@@ -1,17 +1,4 @@
 class CfgRemoteExec {
-    // List of Functions allowed to be sent from client via remoteExec
-    class Functions {
-        mode = 2;           // 0: Completely blocked,   1: Blocked by default,      2: Allow All
-        jip = 1;            // 0: No JIP,               1: JIP Allowed
-        allowedTargets = 0; // 0: All machines,         1: Only to other clients,   2: Only to server
-		
-		class vcm_serverask { allowedTargets = 0;jip = 1; };
-		class VCM_PublicScript { allowedTargets = 0;jip = 1; };
-		class SpawnScript { allowedTargets = 0;jip = 1; };
-		class enableSimulationGlobal { allowedTargets = 0;jip = 1; };
-		class VCM_fnc_KnowAbout { allowedTargets = 0;jip = 1; };	
-    };
-
     // List of Commands allowed to be sent from client via remoteExec
     class Commands {
         mode = 2;           // 0: Completely blocked,   1: Blocked by default,      2: Allow All

3b7642638fbee61dbccca7a1d49ad20574cc9836
Bob-Murphy <Bob-Murphy@users.noreply.github.com>
2022-09-18 18:11:26 +0200

    add expansion info

--- A3A/mod.cpp
+++ A3A/mod.cpp
@@ -1,11 +1,12 @@
 name = "A3 Antistasi";
-author = "A3-Antistasi dev team";
+author = "Antistasi Dev Team";
 logo = "";
 logoOver = "";
 logoSmall = "";
-picture = "";
+picture = "antistasi_overview.paa";
 description = "";
 tooltip = "Antistasi";
-overview = "";
+tooltipOwned = "Antistasi";
+overview = ""<br/><t align='center' size='0.75'>Visit Antistasi on our <t href='https://antistasi.de/'>Website</t> | <t href='http://antistasi.de/discord'>Discord</t> | <t href='https://twitter.com/antistasio'>Twitter</t></t><br/><br/>";
 actionName = "GitHub";
-action = "https://github.com/";
+action = "https://github.com/official-antistasi-community/A3-Antistasi";

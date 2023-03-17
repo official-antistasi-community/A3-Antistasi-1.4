08edf7aca086e27e2bbbe09946053ce06f6e4b30
Killerswin2 <thecakebecamethelie@yahoo.com>
2023-03-16 19:42:17 -0500

    review changes

--- A3A/addons/logistics/Cargo/Vanilla.hpp
+++ A3A/addons/logistics/Cargo/Vanilla.hpp
@@ -175,7 +175,7 @@ class A3_Structures_F_Heli_Ind_Cargo_Cargo10_military_green_F_p3d: TRIPLES(ADDON
 {
     offset[] = {0,0.1,1.35};
     rotation[] = {1,0,0};
-    size = 4;
+    size = 5;
 };
 
 class A3_Structures_F_Heli_Ind_Cargo_Cargo10_red_F_p3d: TRIPLES(ADDON,Cargo,Base)

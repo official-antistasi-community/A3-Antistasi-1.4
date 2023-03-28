775561176f05c0b1dac318ce56de5d144908fd6e
Whiskeysim(Hazey) <1194692+MeltedPixel@users.noreply.github.com>
2023-03-27 20:24:36 -0700

    Restored missing Events Component.

--- A3A/addons/events/Events.hpp
+++ A3A/addons/events/Events.hpp
@@ -79,6 +79,16 @@ class Events {
             };
         };
     };
+    class civVehInit {
+        isLocal = 1;
+        class params {
+            class _0 {
+                description = "civ vehicle being initialized";
+                types[] = {"OBJECT"};
+                optional = 0;
+            };
+        };
+    };
     class locationSpawned {
         isLocal = 1;
         class params {

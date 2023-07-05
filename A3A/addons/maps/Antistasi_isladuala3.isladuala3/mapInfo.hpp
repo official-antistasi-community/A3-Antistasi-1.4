dcb79da57256f8d6381e9c56694678fd4ed4540d
ante <49555217+ante185@users.noreply.github.com>
2023-07-05 22:16:00 +0200

    update

--- A3A/addons/maps/Antistasi_isladuala3.isladuala3/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_isladuala3.isladuala3/mapInfo.hpp
@@ -1,15 +1,15 @@
-class sara {
+class isladuala3 {
 	population[] = {
-		{"Sara_Rashidah",25},{"Sara_Balmopan",97},{"Sara_Estrella",66},{"Sara_Chantico",82},{"Sara_Cedras",33},{"Sara_Ambergris",59},{"Sara_Ortego",450},{"Sara_Cayo",94},{"Sara_Iguana",107},{"Sara_Dolores",315},{"Sara_Parato",26},{"Sara_Tiberia",49},{"Sara_Geraldo",33},{"Sara_Corazol",397},{"Sara_Gulan",38},{"Sara_Somato",167},{"Sara_Bonanza",25},{"Sara_Corinto",31},{"Sara_Yoro",25},{"Sara_Pesto",25},{"Sara_Paraiso",450},{"Sara_Arcadia",74},{"Sara_Masbete",59},{"Sara_Pita",82},{"Sara_Bagango",218},{"Sara_Eponia",109},{"Sara_Everon",59},{"Sara_Hunapu",34},{"Sara_Pacamac",30},{"Sara_Tlaloc",33},{"Sara_Obregan",147},{"Sara_Carmen",33},{"Sara_Ixel",31},{"Sara_Mercalillo",154},{"Sara_Modesta",52},{"Sara_Valor",25},{"Sara_Gaula",45},{"Sara_Mataredo",25},{"Sara_Benoma",25},{"Sara_Tandag",43},{"Sara_Rahmadi",30}
+		{"bolabongo",1055},{"dendala",803},{"kinsella",737},{"canto",680},{"kirabo",429},{"engor",316},{"swonto",254},{"kwako",204},{"djolan",191},{"mawimbela",188},{"tarisol",187},{"numbo",186},{"larenga",165},{"dasha",162},{"orellan",121},{"molisana",115},{"simbala",114},{"maluri",97},{"xibo",95},{"kingala",92},{"pumado",84},{"ursana",63},{"casinda",51},{"zemanovo",46},{"deboor",43},{"kimbaka",36},{"obmeya",109},{"phazena",94},{"pinley",354},{"minesini",100},{"forteza",42},{"upeesdi",156},{"camara",260},{"cainna",59},{"lina",49},{"fishala",15},{"sttrisha",53},{"mangomak",63},{"zeelor",75},{"campolongo",93},{"solon",79},{"lapoto",71},{"vestinga",54},{"ravane",52},{"lukasa",48},{"katakabi",33},{"spookdorp",29},{"mochiwo",26},{"kanis",25},{"yanga",45},{"kiera",30},{"elbakal",28},{"chaba",22},{"nofar",22},{"kaalom",19},{"polani",19},{"cova",17},{"ashlakeresort",16},{"razfook",16},{"machida",14},{"jordin",6}
 	};
 	disabledTowns[] = {}; //no towns that need to be disabled
 	antennas[] = {
-		{3142.96,2739.15,0.18647},{8514.74,7996.98,0.0240936},{11464.1,6307.43,-0.0322723},{11885.1,6210.11,-15.4125},{9617.11,9829.03,0},{10214.7,9348.09,0.0702515},{9738.74,9966.7,-0.226944},{10415.5,9761.01,-0.0189056},{12621.4,7490.31,0.1297},{12560.1,8362.11,-0.157566},{13328.6,9055.83,0.350442},{4940.89,15457.6,-0.18277},{12327.2,15031.4,0},{14788,12762.9,-15.4287},{11068.1,16903.5,-0.0132771},{13964.6,15752.9,-15.429},{17263.3,14160.1,-0.1},{17605.3,9567.2,0},{18921.8,14188.5,-0.000673294},{8197.28,7769.87,0}
+		{5406.87,5050.04,-1.5221},{5821.55,4264.93,-1.54266},{5792.85,6096.85,0},{4794.71,7644.3,-1.54266},{4232.13,2146.34,1.90735e-006}
 	};
 	antennasBlacklistIndex[] = {1,3,4,5,9,11,13,16,17};
 	banks[] = {}; //no suitable building available
 	garrison[] = {
-		{},{"airport_1","seaport_6","outpost_22","outpost_15","resource_9","outpost_19","outpost_14","resource_11"},{},{"control_28","control_27"}
+		{},{"airport_1","seaport_1","resource_1","factory_1","outpost_1","outpost_2","outpost_3","outpost_4","outpost_66"},{},{"control_39","control_100","control_42","control_43","control_104","control_41","control_101","control_40","control_103","control_76","control_102","control_73","control_74","control_75"}
 	};
 	fuelStationTypes[] = {"Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"};
 	climate = "arid";

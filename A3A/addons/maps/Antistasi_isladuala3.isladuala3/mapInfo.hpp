0aaba7bd96c3b2b51ff30bba9271fa984710a6b4
ante <49555217+ante185@users.noreply.github.com>
2023-07-28 02:48:47 +0200

    Update mapInfo.hpp

--- A3A/addons/maps/Antistasi_isladuala3.isladuala3/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_isladuala3.isladuala3/mapInfo.hpp
@@ -2,14 +2,14 @@ class isladuala3 {
 	population[] = {
 		{"bolabongo",1055},{"dendala",803},{"kinsella",737},{"canto",680},{"kirabo",429},{"engor",316},{"swonto",254},{"kwako",204},{"djolan",191},{"mawimbela",188},{"tarisol",187},{"numbo",186},{"larenga",165},{"dasha",162},{"orellan",121},{"molisana",115},{"simbala",114},{"maluri",97},{"xibo",95},{"kingala",92},{"pumado",84},{"ursana",63},{"casinda",51},{"zemanovo",46},{"deboor",43},{"kimbaka",36},{"obmeya",109},{"phazena",250},{"pinley",354},{"minesini",100},{"forteza",42},{"upeesdi",156},{"camara",260},{"cainna",59},{"lina",49},{"fishala",15},{"sttrisha",53},{"mangomak",63},{"zeelor",75},{"campolongo",93},{"solon",79},{"lapoto",71},{"vestinga",54},{"ravane",52},{"lukasa",48},{"katakabi",33},{"spookdorp",29},{"mochiwo",26},{"kanis",25},{"yanga",45},{"kiera",30},{"elbakal",28},{"chaba",25},{"nofar",25},{"kaalom",25},{"polani",25},{"cova",25},{"ashlakeresort",16},{"razfook",16},{"machida",14},{"jordin",6}
 	};
-	disabledTowns[] = {"machida","dasha","upeesdi", "jordin", "razfook", "ashlakeresort"}; //MacHida is an airfield, rest are ruins
+	disabledTowns[] = {"machida","dasha","upeesdi", "jordin", "razfook", "ashlakeresort","kiera"}; //MacHida is an airfield, rest are ruins
 	antennas[] = {
 		{5406.87,5050.04,-1.5221},{5821.55,4264.93,-1.54266},{5792.85,6096.85,0},{4794.71,7644.3,-1.54266},{4232.13,2146.34,1.90735e-006},{2769.18,9614.3,0},{1559.72,5362.99,0},{2662.63,1237.69,0},{532.415,7337.52,0},{8499.49,9836.25,0},{8638.09,362.661,0}
 	};
 	antennasBlacklistIndex[] = {1};
 	banks[] = {}; //no suitable building available (What is a suitable building?)
 	garrison[] = {
-		{},{"airport_1","seaport_1","seaport_9","resource_1","resource_19","factory_1","outpost_1","outpost_2","outpost_3"},{},{"control_39","control_100","control_42","control_43","control_104","control_41","control_101","control_40","control_103","control_76","control_102","control_73","control_74","control_75","control_127","control_128"}
+		{},{"airport_1","seaport_1","seaport_9","resource_1","resource_19","factory_1","outpost_1","outpost_2","outpost_3"},{},{}
 	};
 	fuelStationTypes[] = {"Land_A_FuelStation_Feed","Land_Ind_FuelStation_Feed_EP1","Land_FuelStation_Feed_PMC","Land_Fuelstation","Land_Fuelstation_army","Land_Benzina_schnell"};
 	climate = "arid";

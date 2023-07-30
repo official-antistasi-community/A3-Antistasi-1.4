ca4f86e9aac9e0bf726b0d087d3c19323371ce34
ante <49555217+ante185@users.noreply.github.com>
2023-07-31 01:04:23 +0200

    setup

---
+++ A3A/addons/logistics/Nodes/SFP.hpp
@@ -0,0 +1,126 @@
+class sfp_bv206_sfp_bv206_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};
+class sfp_81_tgb11 : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};
+class sfp_81_tgb1112 : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};
+class sfp_tgb1111_sog_ksp58 : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};
+class sfp_81_tgb13_ksp58 : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};
+class sfp_81_tgb13_ksp58 : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};
+class sfp_hkp16 : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};
+class sfp_hkp16_ffv : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};
+class sfp_hkp4 : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-1.1,-0.7};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.9,-0.7};
+        };
+    };
+};

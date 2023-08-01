122221a504ededc11d5029d3f314ff3f6959302f
ante <49555217+ante185@users.noreply.github.com>
2023-08-01 16:56:43 +0200

    Updated blocked seats

--- A3A/addons/logistics/Nodes/SFP.hpp
+++ A3A/addons/logistics/Nodes/SFP.hpp
@@ -1,5 +1,6 @@
 class sfp_tgb_sfp_tgb13_ksp58 : TRIPLES(ADDON,Nodes,Base)
 {
+    canLoadWeapon = 0;
     class Nodes
     {
         class Node1
@@ -14,45 +15,55 @@ class sfp_tgb_sfp_tgb13_ksp58 : TRIPLES(ADDON,Nodes,Base)
 };
 class sfp_tgb20 : TRIPLES(ADDON,Nodes,Base)
 {
+    canLoadWeapon = 0;
     class Nodes
     {
         class Node1
         {
             offset[] = {0,-0.1,1.05};
+            seats[] = {1,2,3,4};
         };
         class Node2
         {
             offset[] = {0,-0.9,1.05};
+            seats[] = {5,6};
         };
         class Node3
         {
             offset[] = {0,-1.7,1.05};
+            seats[] = {7,8,9,10};
         };
         class Node4
         {
             offset[] = {0,-2.5,1.05};
+            seats[] = {11,12};
         };
         class Node5
         {
             offset[] = {0,-3.3,1.05};
+            seats[] = {13,14,15,16};
         };
     };
 };
 class sfp_bv206_sfp_bv206_p3d : TRIPLES(ADDON,Nodes,Base)
 {
+    canLoadWeapon = 0;
     class Nodes
     {
         class Node1
         {
             offset[] = {0,-1.3,-0.9};
+            seats[] = {3,4,5,6};
         };
         class Node2
         {
             offset[] = {0,-2.1,-0.9};
+            seats[] = {7,8,9,10};
         };
         class Node3
         {
             offset[] = {0,-2.9,-0.9};
+            seats[] = {11,12,13,14};
         };
     };
 };
@@ -62,17 +73,18 @@ class sfp_tgb_sfp_tgb1112_p3d : TRIPLES(ADDON,Nodes,Base)
     {
         class Node1
         {
-            offset[] = {0,-1.2,1.15};
+            offset[] = {0,-1.25,1.15};
         };
     };
 };
+
 class sfp_tgb_sfp_tgb11 : TRIPLES(ADDON,Nodes,Base)
 {
     class Nodes
     {
         class Node1
         {
-            offset[] = {0,-1.2,1.15};
+            offset[] = {0,-1.25,1.15};
         };
     };
 };
@@ -82,12 +94,13 @@ class sfp_tgb_sfp_tgb1111_sog_ksp58 : TRIPLES(ADDON,Nodes,Base)
     {
         class Node1
         {
-            offset[] = {0,-1.2,-0.35};
+            offset[] = {-0.1,-1.15,-0.35};
         };
     };
 };
 class sfp_tgb40_sfp_tgb40_p3d : TRIPLES(ADDON,Nodes,Base)
 {
+    canLoadWeapon = 1;
     class Nodes
     {
         class Node1
@@ -114,6 +127,7 @@ class sfp_tgb40_sfp_tgb40_p3d : TRIPLES(ADDON,Nodes,Base)
 };
 class sfp_tgb40_sfp_tgb30_p3d : TRIPLES(ADDON,Nodes,Base)
 {
+    canLoadWeapon = 1;
     class Nodes
     {
         class Node1
@@ -140,6 +154,7 @@ class sfp_tgb40_sfp_tgb30_p3d : TRIPLES(ADDON,Nodes,Base)
 };
 class sfp_strb90_sfp_strb90h_p3d : TRIPLES(ADDON,Nodes,Base)
 {
+    canLoadWeapon = 0;
     class Nodes
     {
         class Node1
@@ -158,6 +173,7 @@ class sfp_strb90_sfp_strb90h_p3d : TRIPLES(ADDON,Nodes,Base)
 };
 class sfp_hkp16_ffv : TRIPLES(ADDON,Nodes,Base)
 {
+    canLoadWeapon = 0;
     class Nodes
     {
         class Node1
@@ -172,21 +188,25 @@ class sfp_hkp16_ffv : TRIPLES(ADDON,Nodes,Base)
 };
 class sfp_hkp16_sfp_hkp16_p3d : TRIPLES(ADDON,Nodes,Base) //Could be valid for other blackhawks
 {
+    canLoadWeapon = 0;
     class Nodes
     {
         class Node1
         {
-            offset[] = {0,1.3,-1.9};
+            offset[] = {0,1.2,-1.9};
+            seats[] = {1,2,5,6};
         };
     };
 };
 class sfp_gruppbat_sfp_gbat_p3d : TRIPLES(ADDON,Nodes,Base)
 {
+    canLoadWeapon = 0;
     class Nodes
     {
         class Node1
         {
             offset[] = {0,-2.7,-0.81};
+            seats[] = {1,2,5,6};
         };
     };
 };

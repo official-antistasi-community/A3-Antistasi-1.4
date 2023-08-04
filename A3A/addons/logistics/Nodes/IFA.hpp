29e2020afa6836c91d2b5368785020693807962c
stutpip123 <77023662+stutpip123@users.noreply.github.com>
2023-08-04 11:00:06 +0100

    Fix Logistics nodes for Trucks sharing models
    The ammo, fuel, repair variants of the vehicles use the same base models, causing nodes to apply to those vehicles too.
    I have corrected this via changing from models to specific classnames.

--- A3A/addons/logistics/Nodes/IFA.hpp
+++ A3A/addons/logistics/Nodes/IFA.hpp
@@ -1,32 +1,63 @@
-class WW2_Assets_m_Vehicles_WheeledAPC_m_IF_SdKfz_7_p3d : TRIPLES(ADDON,Nodes,Base)
+class LIB_SdKfz_7 : TRIPLES(ADDON,Nodes,Base)
 {
         class Nodes
     {
         class Node1
         {
-            offset[] = {0,-0.5,-0.7};
+            offset[] = {0,-0.5,-0.8};
 			seats[] = {2,3,4,5,9};
         };
         class Node2
         {
-            offset[] = {0,-1.3,-0.7};
+            offset[] = {0,-1.3,-0.8};
 			seats[] = {6,10};
         };
         class Node3
         {
-            offset[] = {0,-2.1,-0.7};
+            offset[] = {0,-2.1,-0.8};
 
         };
         class Node4
         {
-            offset[] = {0,-2.9,-0.7};
+            offset[] = {0,-2.9,-0.8};
 			seats[] = {7,8};
         };
     };
 };
+class LIB_DAK_SdKfz_7 : LIB_SdKfz_7 {};
+class LIB_SdKfz_7_w : LIB_SdKfz_7 {};
 
+class LIB_OpelBlitz_Open_Y_Camo : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.1,-0.04};
+			seats[] = {1,2,7,10,11};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.9,-0.04};
+			seats[] = {5,6};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.7,-0.04};
+			seats[] = {3,4};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.5,-0.04};
+			seats[] = {8,9};
+        };
+    };
+};
+class LIB_DAK_OpelBlitz_Open : LIB_OpelBlitz_Open_Y_Camo {};
+class LIB_OpelBlitz_Open_Y_Camo_w : LIB_OpelBlitz_Open_Y_Camo {};
+class LIB_OpelBlitz_Open_G_Camo_w : LIB_OpelBlitz_Open_Y_Camo {};
 
-class WW2_Assets_m_Vehicles_Trucks_m_IF_Opelblitz_p3d : TRIPLES(ADDON,Nodes,Base)
+class LIB_OpelBlitz_Tent_Y_Camo : TRIPLES(ADDON,Nodes,Base)
 {
 	    canLoadWeapon = 0;
         class Nodes
@@ -53,38 +84,10 @@ class WW2_Assets_m_Vehicles_Trucks_m_IF_Opelblitz_p3d : TRIPLES(ADDON,Nodes,Base
         };
     };
 };
+class LIB_DAK_OpelBlitz_Tent : LIB_OpelBlitz_Tent_Y_Camo {};
+class LIB_OpelBlitz_Tent_Y_Camo_w : LIB_OpelBlitz_Tent_Y_Camo {};
 
-
-class WW2_Assets_m_Vehicles_Trucks_m_IF_Opelblitz_Tent_p3d : TRIPLES(ADDON,Nodes,Base)
-{
-	    canLoadWeapon = 0;
-        class Nodes
-    {
-        class Node1
-        {
-            offset[] = {0,-0.1,-0.04};
-			seats[] = {1,2,7,10,11};
-        };
-        class Node2
-        {
-            offset[] = {0,-0.9,-0.04};
-			seats[] = {5,6};
-        };
-        class Node3
-        {
-            offset[] = {0,-1.7,-0.04};
-			seats[] = {3,4};
-        };
-        class Node4
-        {
-            offset[] = {0,-2.5,-0.04};
-			seats[] = {8,9};
-        };
-    };
-};
-
-
-class WW2_Assets_m_Vehicles_Trucks_m_IF_Us6_p3d : TRIPLES(ADDON,Nodes,Base)
+class LIB_US6_Open : TRIPLES(ADDON,Nodes,Base)
 {
 	    canLoadWeapon = 0;
         class Nodes
@@ -111,9 +114,11 @@ class WW2_Assets_m_Vehicles_Trucks_m_IF_Us6_p3d : TRIPLES(ADDON,Nodes,Base)
         };
     };
 };
+class LIB_US6_Open_Cargo : LIB_US6_Open {};
+class LIB_US6_Tent : LIB_US6_Open {};
+class LIB_US6_Tent_Cargo : LIB_US6_Open {};
 
-
-class WW2_Assets_m_Vehicles_Trucks_m_IF_Zis5v_p3d : TRIPLES(ADDON,Nodes,Base)
+class LIB_Zis5v : TRIPLES(ADDON,Nodes,Base)
 {
         class Nodes
     {
@@ -134,9 +139,9 @@ class WW2_Assets_m_Vehicles_Trucks_m_IF_Zis5v_p3d : TRIPLES(ADDON,Nodes,Base)
         };
     };
 };
+class LIB_Zis5v_w : LIB_Zis5v {};
 
-
-class WW2_Assets_m_Vehicles_Trucks_m_IF_Gmc353Truck_p3d : TRIPLES(ADDON,Nodes,Base)
+class LIB_US_GMC_Open : TRIPLES(ADDON,Nodes,Base)
 {
 	    canLoadWeapon = 0;
         class Nodes
@@ -163,3 +168,8 @@ class WW2_Assets_m_Vehicles_Trucks_m_IF_Gmc353Truck_p3d : TRIPLES(ADDON,Nodes,Ba
         };
     };
 };
+class LIB_US_GMC_Open_w : LIB_US_GMC_Open {};
+class LIB_US_NAC_GMC_Open : LIB_US_GMC_Open {};
+class LIB_US_GMC_Tent : LIB_US_GMC_Open {};
+class LIB_US_NAC_GMC_Tent : LIB_US_GMC_Open {};
+class LIB_US_GMC_Tent_w : LIB_US_GMC_Open {};

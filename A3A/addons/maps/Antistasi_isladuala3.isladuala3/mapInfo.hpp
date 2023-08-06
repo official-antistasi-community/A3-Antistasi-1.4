f24064f5f810b8aaa84c8d9abc82cb4752f0973f
ante <49555217+ante185@users.noreply.github.com>
2023-08-06 17:04:01 +0200

    Update mapInfo.hpp

--- A3A/addons/maps/Antistasi_isladuala3.isladuala3/mapInfo.hpp
+++ A3A/addons/maps/Antistasi_isladuala3.isladuala3/mapInfo.hpp
@@ -2,7 +2,7 @@ class isladuala3 {
 	population[] = {
 		{"bolabongo",1055},{"dendala",803},{"kinsella",737},{"canto",680},{"kirabo",429},{"engor",316},{"swonto",254},{"kwako",204},{"djolan",191},{"mawimbela",188},{"tarisol",187},{"numbo",186},{"larenga",165},{"dasha",162},{"orellan",121},{"molisana",115},{"simbala",114},{"maluri",97},{"xibo",95},{"kingala",92},{"pumado",84},{"ursana",63},{"casinda",51},{"zemanovo",46},{"deboor",43},{"kimbaka",36},{"obmeya",109},{"phazena",250},{"pinley",354},{"minesini",100},{"forteza",42},{"upeesdi",156},{"camara",260},{"cainna",59},{"lina",49},{"fishala",15},{"sttrisha",53},{"mangomak",63},{"zeelor",75},{"campolongo",93},{"solon",79},{"lapoto",71},{"vestinga",54},{"ravane",52},{"lukasa",48},{"katakabi",33},{"spookdorp",29},{"mochiwo",26},{"kanis",25},{"yanga",45},{"kiera",30},{"elbakal",28},{"chaba",25},{"nofar",25},{"kaalom",25},{"polani",25},{"cova",25},{"ashlakeresort",16},{"razfook",16},{"machida",14},{"jordin",6}
 	};
-	disabledTowns[] = {"machida","dasha","upeesdi", "jordin", "razfook", "ashlakeresort","kiera"}; //MacHida is an airfield, rest are ruins
+	disabledTowns[] = {"solon","machida","dasha","upeesdi", "jordin", "razfook", "ashlakeresort","kiera"}; //MacHida is an airfield, rest are ruins
 	antennas[] = {
 		{5406.87,5050.04,-1.5221},{5821.55,4264.93,-1.54266},{5792.85,6096.85,0},{4794.71,7644.3,-1.54266},{4232.13,2146.34,1.90735e-006},{2769.18,9614.3,0},{1559.72,5362.99,0},{2662.63,1237.69,0},{532.415,7337.52,0},{8352.21,8884.22,0},{8638.09,362.661,0},{4317.45,4702.65,0}
 	};

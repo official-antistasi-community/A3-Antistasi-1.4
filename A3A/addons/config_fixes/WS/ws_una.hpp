47fdda2d11f8dfca74776ccf38c92946f686c752
ante <49555217+ante185@users.noreply.github.com>
2023-09-05 21:39:53 +0200

    Vanilla + WS retextures

---
+++ A3A/addons/config_fixes/WS/ws_una.hpp
@@ -0,0 +1,49 @@
+
+class a3a_una_MRAP_01_gmg_F	:	B_MRAP_01_gmg_F
+{
+	side = 1;
+	crew = "B_UN_Soldier_lxWS";
+	faction = "BLU_UN_lxWS";
+	hiddenSelectionsTextures[] = {"lxws\vehicles_f_lxws\data\mrap_01\mrap_01_un_base_co.paa","lxws\vehicles_f_lxws\data\mrap_01\mrap_01_un_adds_co.paa","a3\data_f\vehicles\turret_co.paa"};
+};
+class a3a_una_MRAP_01_hmg_F	:	B_MRAP_01_hmg_F
+{
+	side = 1;
+	crew = "B_UN_Soldier_lxWS";
+	faction = "BLU_UN_lxWS";
+	hiddenSelectionsTextures[] = {"lxws\vehicles_f_lxws\data\mrap_01\mrap_01_un_base_co.paa","lxws\vehicles_f_lxws\data\mrap_01\mrap_01_un_adds_co.paa","a3\data_f\vehicles\turret_co.paa"};
+};
+
+class a3a_una_Offroad_armed	:	B_G_Offroad_01_armed_F
+{
+	side = 1;
+	crew = "B_UN_Soldier_lxWS";
+	faction = "BLU_UN_lxWS";
+	textureList[] = {};
+	hiddenSelectionsTextures[] = {"lxws\vehicles_f_lxws\data\offroad_01\offroad_01_ext_un_co.paa","lxws\vehicles_f_lxws\data\offroad_01\offroad_01_ext_un_co.paa"};
+
+};
+class a3a_una_Offroad_at : B_G_Offroad_01_AT_F
+{
+	side = 1;
+	crew = "B_UN_Soldier_lxWS";
+	faction = "BLU_UN_lxWS";
+	textureList[] = {};
+	hiddenSelectionsTextures[] = {"lxws\vehicles_f_lxws\data\offroad_01\offroad_01_ext_un_co.paa","lxws\vehicles_f_lxws\data\offroad_01\offroad_01_ext_un_co.paa"};
+};
+class a3a_una_Offroad_armor_at : B_Tura_Offroad_armor_AT_lxWS
+{
+	side = 1;
+	crew = "B_UN_Soldier_lxWS";
+	faction = "BLU_UN_lxWS";
+	textureList[] = {};
+	hiddenSelectionsTextures[] = {"lxws\vehicles_f_lxws\data\offroad_01\offroad_01_ext_un_co.paa","lxws\vehicles_f_lxws\data\offroad_01\offroad_01_ext_un_co.paa","lxws\vehicles_f_lxws\offroad_01\data\offroad_01_adds_black_co.paa","lxws\vehicles_f_lxws\offroad_01\data\offroad_01_armor_whiteun_co.paa"};
+};
+class a3a_una_Offroad_armor_armed : B_Tura_Offroad_armor_armed_lxWS
+{
+	side = 1;
+	crew = "B_UN_Soldier_lxWS";
+	faction = "BLU_UN_lxWS";
+	textureList[] = {};
+	hiddenSelectionsTextures[] = {"lxws\vehicles_f_lxws\data\offroad_01\offroad_01_ext_un_co.paa","lxws\vehicles_f_lxws\data\offroad_01\offroad_01_ext_un_co.paa","lxws\vehicles_f_lxws\offroad_01\data\offroad_01_adds_black_co.paa","lxws\vehicles_f_lxws\offroad_01\data\offroad_01_armor_whiteun_co.paa"};
+};

34963df444912ba92de599ece9202c0be8d2f503
ante <49555217+ante185@users.noreply.github.com>
2023-09-05 21:39:53 +0200

    Vanilla + WS retextures

---
+++ A3A/addons/config_fixes/Vanilla/config.cpp
@@ -0,0 +1,22 @@
+#include "..\script_component.hpp"
+
+
+class CfgPatches {
+    class PATCHNAME(A3) {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3_Data_F_AoW_Loadorder"};
+        author = AUTHOR;
+        authors[] = { AUTHORS };
+        authorUrl = "";
+        VERSION_CONFIG;
+    };
+};
+
+// Uncomment when needed
+//#include "CfgMagazines.hpp"
+#include "CfgVehicles.hpp"
+//#include "CfgMarkers.hpp"
+//#include "CfgWeapons.hpp"
---
+++ A3A/addons/config_fixes/WS/config.cpp
@@ -0,0 +1,23 @@
+#include "..\script_component.hpp"
+
+#if __has_include("\lxws\data_f_lxws\config.bin")
+
+class CfgPatches {
+    class PATCHNAME(WS) {
+        name = COMPONENT_NAME;
+        units[] = {};
+        weapons[] = {};
+        requiredVersion = REQUIRED_VERSION;
+        requiredAddons[] = {"A3_Data_F_AoW_Loadorder","Vehicles_F_lxWS"};
+        author = AUTHOR;
+        authors[] = { AUTHORS };
+        authorUrl = "";
+        VERSION_CONFIG;
+    };
+};
+
+// Uncomment when needed
+#include "CfgVehicles.hpp"
+#include "CfgMarkers.hpp"
+
+#endif      // __has_include("\lxws\data_f_lxws\config.bin")

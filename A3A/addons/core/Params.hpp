b81abf6b9f91cc8a18d01d5d36b6d097d56c4341
Bob-Murphy <Bob-Murphy@users.noreply.github.com>
2023-10-28 18:23:02 +0200

    copy-paste error

--- A3A/addons/core/Params.hpp
+++ A3A/addons/core/Params.hpp
@@ -150,7 +150,7 @@ class Params
     };
     class membershipEnabled
     {
-        title = $STR_A3A_Params_TitleMembership_title;
+        title = $STR_A3A_Params_membershipEnabled_title;
         values[] = {0,1};
         texts[] = {$STR_antistasi_dialogs_generic_button_no_tooltip,$STR_antistasi_dialogs_generic_button_yes_text};
         default = 1;

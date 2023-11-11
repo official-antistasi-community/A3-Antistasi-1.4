61a57922da6d969c4e7adf2dcdd64961e52d5811
ante <49555217+ante185@users.noreply.github.com>
2023-11-11 05:01:16 +0100

    Update config.cpp

--- A3A/addons/config_fixes/IslaDuala3/config.cpp
+++ A3A/addons/config_fixes/IslaDuala3/config.cpp
@@ -19,25 +19,26 @@ class CfgPatches
         VERSION_CONFIG;
     };
 };
-//class CfgWorlds {
-//    class IslaDuala3 {
-//        class Names {
-//            class a3a_Obmeya {
-//                angle = 0;
-//                name = "Noord Obmeya";
-//                position[] = {5056.12,5151.08};
-//                radiusA = 314.321;
-//                radiusB = 251.388;
-//                type = "NameCity";
-//            };
-//            //class imalia {
-//            //    angle = 0;                          // never seen this non-zero
-//            //    name = "Imalia";
-//            //    position[] = {8942,620.74};     // 2d map coords
-//            //    radiusA = 300;                      // I think these are supposed to be outer/inner radius but Antistasi uses them as X/Y. Probably a bug.
-//            //    radiusB = 150;
-//            //    type = "NameVillage";               // NameCity also works, I don't think Antistasi cares
-//            //};
-//        };
-//    };
-//};
+class CfgWorlds {
+    class Stratis;
+    class IslaDuala3 : Stratis {
+        class Names {
+            class a3a_Obmeya {
+                angle = 0;
+                name = "Noord Obmeya";
+                position[] = {5056,5151};
+                radiusA = 300;
+                radiusB = 200;
+                type = "NameCity";
+            };
+            class imalia {
+                angle = 0;                          // never seen this non-zero
+                name = "Imalia";
+                position[] = {8942,620.74};     // 2d map coords
+                radiusA = 200;                      // I think these are supposed to be outer/inner radius but Antistasi uses them as X/Y. Probably a bug.
+                radiusB = 150;
+                type = "NameVillage";               // NameCity also works, I don't think Antistasi cares
+            };
+        };
+    };
+};

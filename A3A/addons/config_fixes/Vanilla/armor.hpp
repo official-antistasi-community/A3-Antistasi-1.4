9fb124a4eb97b5ce7689b53a48623e69a6f4a29a
ante <49555217+ante185@users.noreply.github.com>
2023-11-14 18:03:54 +0100

    created 11 more classes

--- A3A/addons/config_fixes/Vanilla/armor.hpp
+++ A3A/addons/config_fixes/Vanilla/armor.hpp
@@ -68,7 +68,6 @@ class APC_Tracked_03_base_F : Tank_F
 	};
 };
 
-class I_APC_Wheeled_03_cannon_F; // !!
 class I_MRAP_03_F;
 class I_MRAP_03_gmg_F;
 class I_MRAP_03_hmg_F;
@@ -90,7 +89,7 @@ class a3a_MRAP_03_hmg_grey_F : I_MRAP_03_hmg_F
 };
 
 //Olive
-class a3a_APC_Wheeled_03_cannon_blufor_F : I_APC_Wheeled_03_cannon_F
+class a3a_APC_Wheeled_03_cannon_blufor_F : a3a_APC_Wheeled_03_cannon_F
 {
     textureList[] = {};
     hiddenSelectionsTextures[] = {"a3\armor_f_gamma\APC_Wheeled_03\data\APC_Wheeled_03_Ext_CO.paa","a3\armor_f_gamma\APC_Wheeled_03\data\APC_Wheeled_03_Ext2_CO.paa","a3\armor_f_gamma\APC_Wheeled_03\data\RCWS30_CO.paa","a3\armor_f_gamma\APC_Wheeled_03\data\APC_Wheeled_03_Ext_alpha_CO.paa","a3\armor_f\data\camonet_aaf_fia_desert_co.paa","a3\armor_f\data\cage_sand_co.paa"};

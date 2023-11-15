e6dc6ecf84c76c4148d2186d875b18c4172a7e52
ante <49555217+ante185@users.noreply.github.com>
2023-11-15 16:53:01 +0100

    WS armour

--- A3A/addons/config_fixes/WS/ws_ifv.hpp
+++ A3A/addons/config_fixes/WS/ws_ifv.hpp
@@ -4,18 +4,98 @@
 //Ion
 class B_ION_APC_Wheeled_01_command_lxWS;
 class B_ION_APC_Wheeled_01_cannon_lxWS;
+class B_ION_APC_Wheeled_01_command_lxWS : B_ION_APC_Wheeled_01_command_lxWS
+{
+    animationList[] = {"showBags",0.5,"showCamonetHull",0,"showCamonetTurret",0,"showSLATHull",1,"showSLATTurret",1};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};
+class B_ION_APC_Wheeled_01_cannon_lxWS : B_ION_APC_Wheeled_01_cannon_lxWS
+{
+    animationList[] = {"showBags",0.5,"showCamonetHull",0,"showCamonetTurret",0,"showSLATHull",1,"showSLATTurret",1};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};
 //NATO
 class APC_Wheeled_01_command_base_lxWS;
 class B_T_APC_Wheeled_01_command_lxWS;
+class a3a_APC_Wheeled_01_command_lxWS : APC_Wheeled_01_command_base_lxWS
+{
+    animationList[] = {"showBags",0.5,"showCamonetHull",0,"showCamonetTurret",0,"showSLATHull",1,"showSLATTurret",1};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};
+class a3a_T_APC_Wheeled_01_command_lxWS : B_T_APC_Wheeled_01_command_lxWS
+{
+    animationList[] = {"showBags",0.5,"showCamonetHull",0,"showCamonetTurret",0,"showSLATHull",1,"showSLATTurret",1};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};
 
 //Iskatel/Kamysh
 //Arid
 class O_APC_Tracked_02_30mm_lxWS;
+class a3a_APC_Tracked_02_30mm_lxWS : O_APC_Tracked_02_30mm_lxWS
+{
+    animationList[] = {"showTracks",0.5,"showCamonetHull",0,"showBags",0.5,"showSLATHull",1};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};
 //Tropical
 class O_T_APC_Tracked_02_30mm_lxWS;
+class a3a_T_APC_Tracked_02_30mm_lxWS : O_T_APC_Tracked_02_30mm_lxWS
+{
+    animationList[] = {"showTracks",0.5,"showCamonetHull",0,"showBags",0.5,"showSLATHull",1};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};
 //SFIA
 class O_SFIA_APC_Tracked_02_30mm_lxWS;
 class O_SFIA_APC_Tracked_02_cannon_lxWS;
+class a3a_SFIA_APC_Tracked_02_30mm_lxWS : O_SFIA_APC_Tracked_02_30mm_lxWS
+{
+    animationList[] = {"showTracks",0.5,"showCamonetHull",0,"showBags",0.5,"showSLATHull",1};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};
+class a3a_SFIA_APC_Tracked_02_cannon_lxWS : O_SFIA_APC_Tracked_02_cannon_lxWS
+{
+    animationList[] = {"showTracks",0.5,"showCamonetHull",0,"showBags",0.5,"showSLATHull",1};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};
 //ION
 class a3a_ION_APC_Tracked_02_30mm : O_APC_Tracked_02_30mm_lxWS
 {

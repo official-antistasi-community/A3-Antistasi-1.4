37454bb1431063d8eb721ee21b4f8bf4101f813a
ante <49555217+ante185@users.noreply.github.com>
2023-11-15 16:41:54 +0100

    Create ws_ifv.hpp

---
+++ A3A/addons/config_fixes/WS/ws_ifv.hpp
@@ -0,0 +1,18 @@
+//WS - ws_ifv.hpp
+
+//Marshall
+//Ion
+class B_ION_APC_Wheeled_01_command_lxWS;
+class B_ION_APC_Wheeled_01_cannon_lxWS;
+//NATO
+class APC_Wheeled_01_command_base_lxWS;
+class B_T_APC_Wheeled_01_command_lxWS;
+
+//Iskatel/Kamysh
+//Arid
+class O_APC_Tracked_02_30mm_lxWS;
+//Tropical
+class O_T_APC_Tracked_02_30mm_lxWS;
+//SFIA
+class O_SFIA_APC_Tracked_02_30mm_lxWS;
+class O_SFIA_APC_Tracked_02_cannon_lxWS;

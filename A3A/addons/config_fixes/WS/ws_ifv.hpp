c2e461d9e9865405c6435eef84134e00c65cb320
ante <49555217+ante185@users.noreply.github.com>
2023-11-15 17:10:02 +0100

    ion atgm

--- A3A/addons/config_fixes/WS/ws_ifv.hpp
+++ A3A/addons/config_fixes/WS/ws_ifv.hpp
@@ -4,6 +4,7 @@
 //Ion
 class B_ION_APC_Wheeled_01_command_lxWS;
 class B_ION_APC_Wheeled_01_cannon_lxWS;
+class B_D_APC_Wheeled_01_atgm_lxWS;
 class a3a_ION_APC_Wheeled_01_command_lxWS : B_ION_APC_Wheeled_01_command_lxWS
 {
     animationList[] = {"showBags",0.5,"showCamonetHull",0,"showCamonetTurret",0,"showSLATHull",1,"showSLATTurret",1};
@@ -23,6 +24,13 @@ class a3a_ION_APC_Wheeled_01_cannon_lxWS : B_ION_APC_Wheeled_01_cannon_lxWS
         init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
         killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
 	};
+};
+class a3a_ION_APC_Wheeled_01_atgm : B_D_APC_Wheeled_01_atgm_lxWS{
+    side = 1;
+    crew = "B_ION_Soldier_lxWS";
+    faction = "BLU_ION_lxWS";
+    textureList[] = {"ION_BLACK", 1};
+    hiddenSelectionsTextures[] = {"lxws\vehicles_f_lxws\data\APC_Wheeled_01\APC_Wheeled_ion_base_CO.paa","lxws\vehicles_f_lxws\data\APC_Wheeled_01\apc_wheeled_ion_adds_co.paa","lxws\vehicles_f_lxws\data\APC_Wheeled_01\apc_wheeled_ion_tows_co.paa","a3\armor_f\Data\camonet_AAF_stripe_desert_CO.paa","lxws\vehicles_f_lxws\data\APC_Wheeled_01\cage_black_CO.paa","lxws\vehicles_f_lxws\data\APC_Wheeled_01\APC_Wheeled_ion_lxws_CO.paa"};
 };
 //NATO
 class APC_Wheeled_01_command_base_lxWS;

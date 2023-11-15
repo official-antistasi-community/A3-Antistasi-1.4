55455a3eaf5ac1951bd3363f5fd0538330419a46
ante <49555217+ante185@users.noreply.github.com>
2023-11-15 16:49:59 +0100

    Update ws_ifv.hpp

--- A3A/addons/config_fixes/WS/ws_ifv.hpp
+++ A3A/addons/config_fixes/WS/ws_ifv.hpp
@@ -16,3 +16,19 @@ class O_T_APC_Tracked_02_30mm_lxWS;
 //SFIA
 class O_SFIA_APC_Tracked_02_30mm_lxWS;
 class O_SFIA_APC_Tracked_02_cannon_lxWS;
+//ION
+class a3a_ION_APC_Tracked_02_30mm : O_APC_Tracked_02_30mm_lxWS
+{
+    side = 1;
+    crew = "B_ION_Soldier_lxWS";
+    faction = "BLU_ION_lxWS";
+    animationList[] = {"showTracks",1,"showCamonetHull",0,"showBags",0.2,"showSLATHull",1};
+    textureList[] = {"Grey", 1};
+    hiddenSelectionsTextures[] = {"lxWS\vehicles_1_f_lxws\APC_Tracked_02\data\APC_Tracked_02_ext_01_black_CO.paa","lxWS\vehicles_1_f_lxws\APC_Tracked_02\data\APC_Tracked_02_ext_02_black_CO.paa","lxWS\vehicles_1_f_lxws\APC_Tracked_02\data\APC_Tracked_02_ext_03_black_CO.paa","lxWS\vehicles_1_f_lxws\APC_Tracked_02\data\APC_Tracked_02_30mm_black_co.paa","A3\Armor_F\Data\camonet_CSAT_Stripe_Desert_CO.paa","lxWS\vehicles_1_f_lxws\APC_Tracked_02\data\cage_black_CO.paa"};
+	class EventHandlers
+	{
+        fired = "_this call (uinamespace getvariable 'BIS_fnc_effectFired');";
+        init = "if (local (_this select 0)) then {[(_this select 0), """", [], false] call bis_fnc_initVehicle;};";
+        killed = "_this call (uinamespace getvariable 'BIS_fnc_effectKilled');";
+	};
+};

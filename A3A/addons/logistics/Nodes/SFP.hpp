ff026d38700aa8b1e1522fed479ef61cd91963c4
ante <49555217+ante185@users.noreply.github.com>
2023-12-18 23:25:28 +0100

    killed some logi nodes

--- A3A/addons/logistics/Nodes/SFP.hpp
+++ A3A/addons/logistics/Nodes/SFP.hpp
@@ -45,28 +45,6 @@ class sfp_tgb20 : TRIPLES(ADDON,Nodes,Base)
         };
     };
 };
-class sfp_bv206_sfp_bv206_p3d : TRIPLES(ADDON,Nodes,Base)
-{
-    canLoadWeapon = 0;
-    class Nodes
-    {
-        class Node1
-        {
-            offset[] = {0,-1.3,-0.9};
-            seats[] = {3,4,5,6};
-        };
-        class Node2
-        {
-            offset[] = {0,-2.1,-0.9};
-            seats[] = {7,8,9,10};
-        };
-        class Node3
-        {
-            offset[] = {0,-2.9,-0.9};
-            seats[] = {11,12,13,14};
-        };
-    };
-};
 class sfp_tgb_sfp_tgb1112_p3d : TRIPLES(ADDON,Nodes,Base)
 {
     class Nodes
@@ -168,33 +146,6 @@ class sfp_strb90_sfp_strb90h_p3d : TRIPLES(ADDON,Nodes,Base)
         class Node3
         {
             offset[] = {0,-5.5,-1.15};
-        };
-    };
-};
-class sfp_hkp16_ffv : TRIPLES(ADDON,Nodes,Base)
-{
-    canLoadWeapon = 0;
-    class Nodes
-    {
-        class Node1
-        {
-            offset[] = {0,2.1,-1.9};
-        };
-        class Node2
-        {
-            offset[] = {0,1.3,-1.9};
-        };
-    };
-};
-class sfp_hkp16_sfp_hkp16_p3d : TRIPLES(ADDON,Nodes,Base) //Could be valid for other blackhawks
-{
-    canLoadWeapon = 0;
-    class Nodes
-    {
-        class Node1
-        {
-            offset[] = {0,1.2,-1.9};
-            seats[] = {1,2,5,6};
         };
     };
 };

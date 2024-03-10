7fe9399c517f958f03a5ad3992e589376c51e716
ante <49555217+ante185@users.noreply.github.com>
2024-03-10 15:41:57 +0100

    IFA logistics

---
+++ A3A/addons/logistics/Nodes/IFA.hpp
@@ -0,0 +1,136 @@
+class WW2_Assets_m_Vehicles_Cars_m_IF_Willys_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {-0.325,-0.9,-0.35};
+        };
+    };
+};
+
+class WW2_Assets_m_Vehicles_Trucks_m_IF_Opelblitz_Tent_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.4,-0.05};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.2,-0.05};
+        };
+        class Node3
+        {
+            offset[] = {0,-2,-0.05};
+        };
+    };
+};
+
+class WW2_Assets_m_Vehicles_Trucks_m_IF_Opelblitz_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.4,-0.05};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.2,-0.05};
+        };
+        class Node3
+        {
+            offset[] = {0,-2,-0.05};
+        };
+    };
+};
+
+class WW2_Assets_m_Vehicles_WheeledAPC_m_IF_SdKfz_7_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.5,-0.75};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.3,-0.75};
+        };
+        class Node3
+        {
+            offset[] = {0,-2.1,-0.75};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.9,-0.75};
+        };
+    };
+};
+
+class WW2_Assets_m_Vehicles_Trucks_m_IF_Us6_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.4,0.2};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.2,0.2};
+        };
+        class Node3
+        {
+            offset[] = {0,-2,0.2};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.8,0.2};
+        };
+    };
+};
+
+class WW2_Assets_m_Vehicles_Trucks_m_IF_Gmc353Truck_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.4,-0.6};
+        };
+        class Node2
+        {
+            offset[] = {0,-1.2,-0.6};
+        };
+        class Node3
+        {
+            offset[] = {0,-2,-0.6};
+        };
+        class Node4
+        {
+            offset[] = {0,-2.8,-0.6};
+        };
+    };
+};
+
+class WW2_Assets_m_Vehicles_Trucks_m_IF_Zis5v_p3d : TRIPLES(ADDON,Nodes,Base)
+{
+        class Nodes
+    {
+        class Node1
+        {
+            offset[] = {0,-0.15,-0.4};
+        };
+        class Node2
+        {
+            offset[] = {0,-0.95,-0.4};
+        };
+        class Node3
+        {
+            offset[] = {0,-1.75,-0.4};
+        };
+    };
+};

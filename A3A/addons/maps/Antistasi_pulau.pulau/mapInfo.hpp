21cbe53bdec6fe03ef266402d85fe8f7d3f99b1b
ante <49555217+ante185@users.noreply.github.com>
2024-04-06 06:46:03 +0200

    initial commit

---
+++ A3A/addons/maps/Antistasi_pulau.pulau/mapInfo.hpp
@@ -0,0 +1,61 @@
+class pulau {
+	population[] = {
+        {"DefaultKeyPoint1",38},
+        {"DefaultKeyPoint2",59},
+        {"DefaultKeyPoint3",76},
+        {"DefaultKeyPoint4",41},
+        {"DefaultKeyPoint5",57},
+        {"DefaultKeyPoint6",38},
+        {"DefaultKeyPoint7",43},
+        {"DefaultKeyPoint8",28},
+        {"DefaultKeyPoint10",47},
+        {"DefaultKeyPoint11",57},
+        {"DefaultKeyPoint12",26},
+        {"DefaultKeyPoint13",27},
+        {"DefaultKeyPoint9",47},
+        {"DefaultKeyPoint14",26},
+        {"DefaultKeyPoint15",31},
+        {"DefaultKeyPoint16",25},
+        {"DefaultKeyPoint17",25},
+        {"DefaultKeyPoint18",23},
+        {"DefaultKeyPoint19",33},
+        {"DefaultKeyPoint20",64},
+        {"DefaultKeyPoint21",34},
+        {"DefaultKeyPoint22",27},
+        {"DefaultKeyPoint23",58},
+        {"DefaultKeyPoint24",30},
+        {"DefaultKeyPoint25",38},
+        {"DefaultKeyPoint26",33},
+        {"DefaultKeyPoint27",59},
+        {"DefaultKeyPoint28",52},
+        {"DefaultKeyPoint29",20},
+        {"DefaultKeyPoint30",22},
+        {"DefaultKeyPoint31",32},
+        {"DefaultKeyPoint32",31},
+        {"Loholoho",233}
+    };
+	disabledTowns[] = {};
+	antennas[] = {
+		{2682.94,2592.64,-0.000686646}
+    };
+	antennasBlacklistIndex[] = {};
+	banks[] = {
+	};
+	garrison[] = {
+		{},{"seaport_1","outpost_1","outpost_18"},{},{"control_0","control_1","control_2","control_10","control_11"}
+	};
+	fuelStationTypes[] = {
+		"Land_FuelStation_Feed_F","Land_fs_feed_F","Land_FuelStation_01_pump_malevil_F","Land_FuelStation_01_pump_F","Land_FuelStation_02_pump_F","Land_FuelStation_03_pump_F"
+	};
+	climate = "tropical";
+	buildObjects[] = {
+		// Large vanilla tropical structures
+		{"Land_BagBunker_01_large_green_F", 300}, {"Land_HBarrier_01_tower_green_F", 300}, {"Land_BagBunker_01_small_green_F", 60},
+		{"Land_Shed_09_F", 120}, {"Land_Shed_10_F", 140},
+		// vanilla tropical sandbag walls
+		{"Land_BagFence_01_short_green_F", 10}, {"Land_BagFence_01_round_green_F", 10}, {"Land_BagFence_01_long_green_F", 10},
+		// Non-camo vanilla stuff
+		{"Land_SandbagBarricade_01_half_F", 20}, {"Land_SlumWall_01_s_2m_F", 5}, {"Land_PillboxBunker_01_hex_F", 200},
+		{"Land_Barricade_01_4m_F", 30}, {"Land_GuardBox_01_brown_F", 80}, {"Land_Tyres_F", 10}
+	};
+};

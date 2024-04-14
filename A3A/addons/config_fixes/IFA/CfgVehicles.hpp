518127774f240d09c0658446675d5e1ea12f12ef
ante <49555217+ante185@users.noreply.github.com>
2024-04-14 20:14:48 +0200

    Update CfgVehicles.hpp

--- A3A/addons/config_fixes/IFA/CfgVehicles.hpp
+++ A3A/addons/config_fixes/IFA/CfgVehicles.hpp
@@ -2,4 +2,12 @@
 
 class CfgVehicles 
 {
+	class LIB_Zis6_Parm;
+	class a3a_lib_Zis6_BOX : LIB_Zis6_Parm {
+		displayName = "ZIS-5V (Box)";
+		transportRepair = 0;
+		typicalCargo[] = {"LIB_FFI_LAT_Soldier"};
+		faction = "LIB_FFI";
+		side = 2;
+	};
 };

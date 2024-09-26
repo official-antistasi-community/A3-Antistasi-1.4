9e5842ecff15f530e16138ff4fc5bd20ba9acdbd
ante <49555217+ante185@users.noreply.github.com>
2024-09-26 20:44:07 +0200

    Update CfgWeapons.hpp

--- A3A/addons/config_fixes/WS/CfgWeapons.hpp
+++ A3A/addons/config_fixes/WS/CfgWeapons.hpp
@@ -7,4 +7,9 @@ class CfgWeapons
     {
         baseWeapon = "arifle_VelkoR5_GL_lxWS";
     };
+    class arifle_VelkoR5_lxWS;
+	class srifle_GM6_F;
+	class srifle_GM6_snake_lxWS : srifle_GM6_F {
+        baseWeapon = "srifle_GM6_snake_lxWS";
+	};
 };
